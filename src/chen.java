25cbb4062c7b286521a03044e8c315fbbfb0db2e
3570872073 <3570872073@qq.com>
2024-04-25 14:55:00 +0800

    本地库第一次提交

---
+++ src/chen.java
@@ -0,0 +1,7 @@
+public class chen {
+    public static void main(String[] args) {
+        System.out.println("陈佳伟");
+        System.out.println("22物联网01");
+        System.out.println("2022133268");
+    }
+}
